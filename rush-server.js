da1b8a0659380b78cf88f29676716d170afa9a74
Iuliu Visovan <Iuliu Visovan>
2017-01-21 21:45:32 +0200

    New version, with some images

--- rush-server.js
+++ rush-server.js
@@ -1,7 +1,14 @@
 var players = [];
+var existingWords;
+
+const fs = require('fs');
+const ImagesClient = require('google-images');
+const googleClient = new ImagesClient('012529666448905206368:ldg-wfa5wmc', 'AIzaSyCZiHDBCcIzdqKa1SL0XBlxtUUcf_VqO-c');
+
 module.exports = {
     init: (io) => {
         io.on('connection', socket => {
+            // setWordImages();
             players.push({
                 id: socket.id,
                 name: '<some unknown guy>',
@@ -29,21 +36,21 @@ module.exports = {
                 socket.broadcast.emit('go');
                 isGameStarted = true;
                 io.emit('questionArrived', JSON.stringify({
-                    question: questions[questions.length - 1],
+                    question: questions[questions.length - 1].imageUrl,
                     currentPlayerScore: 0
                 }));
             });
             socket.on('answerPush', response => {
                 var currentPlayer = findPlayer(socket.id);
-                // currentPlayer.score = currentPlayer.score + (response == eval(questions.pop()) ? 1 : 0);
-                currentPlayer.score = currentPlayer.score + 1
+                var isRightAnswer = response.toLowerCase().trim() == questions.pop().word.toLowerCase().trim();
+                currentPlayer.score = currentPlayer.score + (isRightAnswer ? 1 : 0);
                 io.emit('playerlistupdate', J(players));
                 if (currentPlayer.score > 4) {
                     io.emit('end', currentPlayer.name);
                     return;
                 }
                 socket.emit('questionArrived', JSON.stringify({
-                    question: questions[questions.length - 1],
+                    question: questions[questions.length - 1].imageUrl,
                     currentPlayerScore: currentPlayer.score
                 }));
             });
@@ -53,7 +60,6 @@ module.exports = {
 
 
 
-var players = [];
 var isGameStarted = false;
 var findPlayer = (id) => players.find(x => x.id == id);
 var J = (string) => JSON.stringify(string);
@@ -62,11 +68,42 @@ var questions = [];
 var operators = ['+', '-'];
 var initQuestions = () => {
     questions = [];
-    for (var i = 0; i < 100; i++) {
-        let x = i;
-        let randomNumber1 = (new Date().getTime() + x % 23) % 99;
-        let randomNumber2 = (new Date().getTime() + x % 16) % 99;
-        let operator = operators[(new Date().getTime() + x % 9) % operators.length];
-        questions.push(`${randomNumber1}${operator}${randomNumber2}`);
+    var availableWords = JSON.parse(fs.readFileSync('words.json', 'utf8'));
+    for (var i = 0; i < 50; i++) {
+        var randNum = Math.floor(Math.random() * (availableWords.length - 1));
+        if (!questions.some(x => x.word == availableWords[randNum].word))
+            questions.push(availableWords[randNum]);
     }
+}
+
+var setWordImages = () => {
+    rawWords = fs.readFileSync('rawwords.txt', 'utf8');
+    existingWords = JSON.parse(fs.readFileSync('words.json', 'utf8'));
+    var updatedWords = rawWords.split("\n")
+        .map(x => x.trim())
+        .filter(x => x.length)
+        .map(wordString => {
+            return {
+                word: wordString,
+                imageUrl: (existingWords.find(x => x.word == wordString) && existingWords.find(x => x.word == wordString).imageUrl) || ""
+            }
+        });
+    debugger;
+    Promise.all(updatedWords
+            .filter(word => (word.word && word.word.length) && (!word.imageUrl || !word.imageUrl.length))
+            .map(word => updateImageForWord(word)))
+        .then(() => {
+            fs.writeFile('./words.json', JSON.stringify(updatedWords, null, 4), function (err) {
+                console.log("Word images updated succesfully!");
+            });
+        })
+}
+
+function updateImageForWord(word) {
+    return new Promise((resolve, reject) => {
+        googleClient.search(word.word).then(images => {
+            word.imageUrl = images[0].url;
+            resolve();
+        }).catch(err => console.dir(err));
+    });
 }
